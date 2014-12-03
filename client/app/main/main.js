561e6da908b36337ae3e338cc1f4c78cf5035de1
Stickman <stocktonmanners@yahoo.ca>
2014-12-02 20:45:45 -0700

    base M.E.A.N. stack

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('newyappeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
